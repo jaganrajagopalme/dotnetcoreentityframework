fe90f2383c42a1492a2a7b50f963d5671479b1f3
Jagan <Jaganrajagopalme@gmail.com>
2020-04-01 20:41:51 +0530

    Entity framework using dotnet core

---
+++ entityframeworkDotnetcore/entityframeworkDotnetcore/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace entityframeworkDotnetcore.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        
+        [Column("nvarchar(50)")]
+        public string EmployeName { get; set; }
+        [Column("nvarchar(50)")]
+        public string Position { get; set; }
+        [Column("nvarchar(50)")]
+        public string OfficeLocation { get; set; }
+    }
+}
---
+++ entityframeworkDotnetcore/entityframeworkDotnetcore/Models/EmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace entityframeworkDotnetcore.Models
+{
+    public class EmployeeContext:DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options): base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+      
+
+    }
+}
